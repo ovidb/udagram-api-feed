e07d7663b3e19aa992fcec1f6f3dcce4503557b5
ovidb <antarexis@gmail.com>
2020-06-22 03:20:19 +0100

    Remove Users API

--- src/controllers/v0/index.router.ts
+++ src/controllers/v0/index.router.ts
@@ -5,7 +5,6 @@ import {UserRouter} from './users/routes/user.router';
 const router: Router = Router();
 
 router.use('/feed', FeedRouter);
-router.use('/users', UserRouter);
 
 router.get('/', async (req: Request, res: Response) => {
   res.send(`V0`);
